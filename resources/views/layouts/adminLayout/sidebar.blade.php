ada2de534cbf7b27b51e19ae0b1ee0c9d479f834
Jerald Babor <jeraldbabor@gmail.com>
2025-07-24 13:35:54 +0800

    new update

--- resources/views/layouts/adminLayout/sidebar.blade.php
+++ resources/views/layouts/adminLayout/sidebar.blade.php
@@ -2,17 +2,10 @@
     <nav x-data="sidebar" class="sidebar fixed bottom-0 top-0 z-50 h-full min-h-screen w-[260px] shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] transition-all duration-300">
         <div class="h-full bg-white dark:bg-[#0e1726] flex flex-col">
             <!-- Logo and Collapse Button -->
-            <div class="flex items-center justify-between px-4 py-3 border-b border-gray-200 dark:border-gray-700">
-                <a href="index.html" class="main-logo flex shrink-0 items-center">
-                    <img class="ml-[10px] w-20 flex-none" src="{{ asset('/1.jfif') }}" alt="iBarangay Logo">
-                    <span class="align-middle text-2xl font-semibold ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light lg:inline">iBarangay</span>
+            <div class="flex flex-col items-center justify-center px-4 py-3 border-b border-gray-200 dark:border-gray-700">
+                <a href="index.html" class="main-logo flex items-center justify-center">
+                    <img class="w-20" src="{{ asset('/1.jfif') }}" alt="iBarangay Logo">
                 </a>
-                <button type="button" class="collapse-icon flex h-8 w-8 items-center justify-center rounded-full transition duration-300 hover:bg-gray-500/10 rtl:rotate-180 dark:text-white-light dark:hover:bg-dark-light/10" @click="$store.app.toggleSidebar()">
-                    <svg class="h-5 w-5" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M13 19L7 12L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path opacity="0.5" d="M16.9998 19L10.9998 12L16.9998 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
             </div>
 
             <!-- Menu Items -->
